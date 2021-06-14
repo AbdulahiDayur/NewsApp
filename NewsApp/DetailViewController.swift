894c5c1541ff73796b8dcfc34b77bd80799c0d50
Abdul Dayur <Abdul.dayur@nike.com>
2021-06-14 15:46:53 -0700

    created detail vc for a separate display window

---
+++ NewsApp/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  NewsApp
+//
+//  Created by Abdul Dayur on 6/14/21.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
